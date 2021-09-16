446267601f8fb2c09bd331b99167aa208084c5a4
Stephen Ndubueze <sundubueze78@gmail.com>
2021-09-16 14:11:17 -0500

    theme change persists new photo new introduction

--- public_html/functions.js
+++ public_html/functions.js
@@ -1,7 +1,14 @@
 function myLightFunction() {
     document.body.style.backgroundColor = "white";
+    localStorage.setItem("bg", document.body.style.backgroundColor);
 }
 
 function myDarkFunction() {
     document.body.style.backgroundColor = "DarkGray";
+    localStorage.setItem("bg", document.body.style.backgroundColor);
+
+}
+
+window.onload = function () {
+    document.body.style.backgroundColor = localStorage.getItem("bg");
 }
